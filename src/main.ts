b5c894ef1583da09c53b240e9e38cce3c9e401cc
tankbottoms <m@tankbottoms.xyz>
2022-11-14 07:26:37 -0500

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-unresolved */
+import './app.css';
+import App from './App.svelte';
+
+const app = new App({
+	target: document.getElementById('app'),
+});
+
+export default app;
---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true,
+		}),
+	},
+};
